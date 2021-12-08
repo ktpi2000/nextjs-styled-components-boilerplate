7a082aa372e7ec410bc193024ea203c6fe70bfed
ktpi2000 <katapi2000@gmail.com>
2021-12-08 19:22:22 +0900

    formatと不要な画像を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Hello from "../components/Hello";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Hello from '../components/Hello';
 
 const Home = () => {
   return <Hello />;
