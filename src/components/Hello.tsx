b5802a58685a8cb747dd7b9be235bea128127619
ktpi2000 <katapi2000@gmail.com>
2021-12-08 19:08:53 +0900

    src/以下にcomponentsやpagesを移動させる

---
+++ src/components/Hello.tsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <h1>hello</h1>;
+};
+
+export default Hello;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Hello from "../components/Hello";
+
+const Home = () => {
+  return <Hello />;
+};
+
+export default Home;
